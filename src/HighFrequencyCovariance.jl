fcdd7fe1c66f110125e6bc8f35efbcf6d01f8266
s-baumann <stuart@stuartbaumann.com>
2021-01-06 22:56:48 +0000

    Exporting is_psd_matrix

--- src/HighFrequencyCovariance.jl
+++ src/HighFrequencyCovariance.jl
@@ -15,7 +15,7 @@ export SortedDataFrame, CovarianceMatrix, +, -
 export get_correlation, get_volatility
 export make_nan_covariance_matrix, duration
 export subset_to_tick, subset_to_time, calculate_mean_abs_distance
-export to_dataframe, valid_correlation_matrix
+export to_dataframe, valid_correlation_matrix, is_psd_matrix
 export ticks_per_asset, get_assets
 include("01_helpers.jl")
 export simple_differencing
