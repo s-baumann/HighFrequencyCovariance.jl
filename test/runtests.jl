40a797a2e987369e88b65a5559f000ea42e01905
s-baumann <stuart@stuartbaumann.com>
2021-01-08 01:29:57 +0000

    Make runtests file clearer

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,17 @@ using Test
 
 # Run tests
 
-println("TestHighFrequencyCovariance")
-@time @test include("main_tests_syncronous.jl")
-@time @test include("main_tests_asyncronous.jl")
-@time @test include("test_31_Higham.jl")
+println("Test various lowlevel functions")
 @time @test include("test_lowlevel_functions.jl")
+
+println("Test combining of covariance matrices")
 @time @test include("main_tests_combine.jl")
+
+println("Test finding nearest correlation matrix")
+@time @test include("test_31_Higham.jl")
+
+println("Test on syncronous observations")
+@time @test include("main_tests_syncronous.jl")
+
+println("Test on asyncronous observations")
+@time @test include("main_tests_asyncronous.jl")
